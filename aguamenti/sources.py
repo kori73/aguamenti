2279e61c0cf4a0641b023674c1e507d503de4f0e
koray beyaz <koray.beyaz73@gmail.com>
2022-02-16 18:20:45 +0300

    use dask to create larger than memory df
    update tests for dask
    
    fix typo
    
    lint fix

--- aguamenti/sources.py
+++ aguamenti/sources.py
@@ -1,7 +1,6 @@
-from venv import create
 import numpy as np
 import pandas as pd
-
+import dask.dataframe as dd
 
 from .categorical import create_all_hierarchy
 
@@ -42,26 +41,29 @@ class DataGenerator:
         self.repeat = len(self.hierarchy_df)
         self.partition_col = partition_col
 
-    def generate_dates(self):
-        return self.dates.repeat(self.repeat)
+    def generate_dates(self, repeat):
+        return self.dates.repeat(repeat)
 
     def generate_categorical(self):
         return create_all_hierarchy(self.hierarchy)
 
-    def generate_timeseries(self):
+    def generate_timeseries(self, repeat):
         return np.concatenate(
-            [fourier(size=self.unit_rows) for i in range(self.repeat)]
+            [fourier(size=self.unit_rows) for i in range(repeat)]
         )
 
-    def split_by_partition_col(self):
+    def get_distinct_partition_vals(self):
         df = self.hierarchy_df
-        partition_vals = df[self.partition_col].unique()
-        return (df[df[self.partition_col] == i] for i in partition_vals)
+        return sorted(df[self.partition_col].unique())
 
-    def generate(self, hierarchy_df):
+    def generate(self, partition=None):
+        if partition is None:
+            hierarchy_df = self.hierarchy_df
+        else:
+            hierarchy_df = self.hierarchy_df[self.hierarchy_df[self.partition_col] == partition]
         categoricals = pd.concat([hierarchy_df] * self.unit_rows).reset_index(drop=True)
-        df = pd.DataFrame({"date": self.generate_dates()})
-        df["y"] = self.generate_timeseries()
+        df = pd.DataFrame({"date": self.generate_dates(len(hierarchy_df))})
+        df["y"] = self.generate_timeseries(len(hierarchy_df))
         df = pd.concat([df, categoricals], axis=1)
         return df
 
@@ -70,9 +72,12 @@ class DataGenerator:
             return self.generate(self.hierarchy_df)
 
         if self.partition_col in self.hierarchy_df.columns:
-            hierarchy_gen = self.split_by_partition_col()
-            results = []
-            for df in hierarchy_gen:
-                self.repeat = len(df)
-                results.append(self.generate(df))
-            return results
+            dsk = {}
+
+            distinct_partitions = self.get_distinct_partition_vals()
+            divisions = list(distinct_partitions)
+            divisions = divisions + [divisions[-1]]
+            for partition_val in distinct_partitions:
+                dsk[("generate", partition_val)] = (self.generate, partition_val)
+            meta = self.generate(distinct_partitions[0])
+            return dd.DataFrame(dsk, "generate", meta, divisions)
--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,3 +1,5 @@
+import dask.dataframe as dd
+
 from aguamenti.sources import DataGenerator
 
 
@@ -10,5 +12,20 @@ def test_data_generator():
     gen = DataGenerator("2020-01-01", "2021-01-01", hierarchy)
     res = gen.generate()
     expected = 1468
+    assert len(res) == expected
+    assert len(res.drop_duplicates(["date", "item", "store"])) == expected
+
+
+def test_data_generator_partitioned():
+    hierarchy = {
+        "item": {"unique_count": 2, "dependencies": set()},
+        "store": {"unique_count": 2, "dependencies": set()},
+        "item_group": {"unique_count": 1, "dependencies": {"item"}}
+    }
+    gen = DataGenerator("2020-01-01", "2021-01-01", hierarchy, partition_col="item_group")
+    res = gen.main()
+    expected = 1468
+    assert isinstance(res, dd.DataFrame)
+    assert res.npartitions == 1
     assert len(res) == expected
     assert len(res.drop_duplicates(["date", "item", "store"])) == expected
